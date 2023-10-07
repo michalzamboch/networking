e2290de9fcdd6d0df72dfc66cdd84eb06dbf2d49
Michal Žamboch <zamboch.michal@seznam.cz>
2023-10-07 20:29:59 +0200

    Serializing Message structure.

--- client/src/main.rs
+++ client/src/main.rs
@@ -18,11 +18,10 @@ fn get_mock_message() -> Message {
 
 fn main() -> io::Result<()> {
     let mut buffer = [0; 1024];
-
     let mut stream = TcpStream::connect("127.0.0.1:8080")?;
 
-    let message = b"Hello, server!";
-    stream.write_all(message)?;
+    let serialized = serde_json::to_vec(&get_mock_message()).unwrap();
+    stream.write_all(&serialized)?;
 
     let bytes_read = stream.read(&mut buffer)?;
     println!(
